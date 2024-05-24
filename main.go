5d2f6c76b0d6d340b32cdae7cdb05034c8f00978
lucasherlon <lucasherlondsmc@gmail.com>
2024-05-23 22:45:42 -0300

    starting the connection with the API

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+    "log"
+    "os"
+    "fmt"
+    "context"
+
+    "github.com/google/generative-ai-go/genai"
+    "google.golang.org/api/option"
+    "github.com/joho/godotenv"
+ )
+
+func main(){
+
+  erro := godotenv.Load(".env") 
+
+  if erro != nil {
+    fmt.Println("Error loading .env file:", erro)
+
+  }
+
+  ctx := context.Background()
+
+  client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_KEY")))
+  if err != nil {
+    log.Fatal(err)
+  }
+  defer client.Close()
+
+  model := client.GenerativeModel("gemini-1.5-flash-latest")
+
+  resp, err := model.GenerateContent(ctx, genai.Text("Quais a vantagens de se usar programação funcional?"))
+  
+  if err != nil {
+    log.Fatal(err)
+  }
+  
+  printResponse(resp)
+
+}
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
